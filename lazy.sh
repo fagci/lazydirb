e9bca6275d3809d3e66ffee96bf7c659f35fa2eb
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-29 03:41:22 +0700

    improvements

--- lazy.sh
+++ lazy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
+DIR='/wp-content/uploads/'
+
 BL_IPS='10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,224-255.-.-.-'
 
 function gen() {
-    nmap -n -iR 102400 -sL --exclude $BL_IPS \
+    nmap -n -iR 1000000 -sL --exclude $BL_IPS \
         | awk '/report for/ {print $NF}'
 }
 
@@ -10,21 +13,23 @@ function dl() {
     local ip="$1"
     wget -q -e robots=off -r \
         -np -nd \
-        -Ajpg,png \
+        -Ajpg,png,gif,jpeg \
         -P "out/$ip/" \
-        "http://$ip/wp-content/uploads/"
+        "http://$ip$DIR"
 }
 
 function check() {
     local ip="$1"
-    curl -s -H 'User-Agent: Mozilla/5.0' \
-        --connect-timeout 2 \
-        --max-time 3 \
-        "http://$ip/wp-content/uploads/" \
+    curl -s -A 'Mozilla/5.0' \
+        --connect-timeout 1 \
+        --max-time 5 \
+        --max-filesize 20K \
+        --tcp-nodelay \
+        "http://$ip$DIR" \
         | fgrep 'Index of' > /dev/null
 }
 
 export -f check
 export -f dl
 
-gen | xargs -P 2048 -I {} bash -c 'check {} && echo {} && dl {}'
+gen | xargs -P 1024 -I {} bash -c 'check {} && echo {} && dl {}'
