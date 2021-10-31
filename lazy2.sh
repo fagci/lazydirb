208513a2fc6b35ee12ce0f6b9eb4dc9b61c07319
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-31 15:01:31 +0700

    optimize

--- lazy2.sh
+++ lazy2.sh
@@ -13,26 +13,26 @@ log() { echo $(date +%H:%M:%S) "$@" 1>&2; }
 
 get_webservers() {
     log 'Start'
-    nmap -n -Pn -T4 -iR $SCAN_COUNT -p $PORT \
-        --host-timeout 1s --min-parallelism $SCAN_W --min-rate $SCAN_W --max-retries 1 \
+    nmap -n -Pn -T5 -iR $SCAN_COUNT -p $PORT \
+        --host-timeout 1s --min-parallelism $SCAN_W --max-retries 1 \
         --open -oG - 2>/dev/null \
         | awk '/\/open\//{print $2}'
     log 'Got ips'
 }
 
 check() {
-    local uri="${SCHEME}://${1}${T_PATH}"
+    local uri="${SCHEME}://${1}:${PORT}${T_PATH}"
     curl -sm 5 -A 'Mozilla/5.0' "$uri" \
         | fgrep "${T_RESPONSE}" >/dev/null
 }
 
 download() {
-    local uri="${SCHEME}://${1}${T_PATH}"
+    local uri="${SCHEME}://${1}:${PORT}${T_PATH}"
     local out_path="out/$1/"
     wget -q -e robots=off -r -np -nd -A "${EXTENSIONS}" -P "$out_path" "$uri"
 }
 
 export -f check download
-export SCHEME T_PATH T_RESPONSE EXTENSIONS
+export SCHEME PORT T_PATH T_RESPONSE EXTENSIONS
 
 get_webservers | xargs -P $CHECK_W -I {} bash -c 'check {} && echo {} && download {}'
