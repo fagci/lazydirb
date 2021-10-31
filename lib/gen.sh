03e0a0e32f5d901e95e53eb704f9192ab630b519
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-01 00:13:52 +0700

    refactor

--- lib/gen.sh
+++ lib/gen.sh
@@ -1,9 +1,17 @@
-random_sites() {
-    local port="${1:-80}"
+random_ips() {
+    local port="${1}"
     local workers="${2:-1500}"
+
+    if [ -z "$port" ]; then
+        echo 'Port not specified' 1>&2
+        exit 255
+    fi
+
     nmap -T5 --min-parallelism "$workers" --max-retries 1 \
         -n -Pn -iR 0 -p "$port" --open -oG - 2>/dev/null \
         | awk '/open/{print $2}'
 }
+
+export random_ips
 
 # vi: ft=bash
