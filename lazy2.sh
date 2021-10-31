6ae89f53ac0c238749bf5f4261a2dbf6e2c69772
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-31 17:59:33 +0700

    refactor

--- lazy2.sh
+++ lazy2.sh
@@ -3,21 +3,22 @@
 # Download images from random wordpress sites
 
 readonly EXTENSIONS='jpg,png,gif,jpeg'
+readonly PARALLEL_SITES_DOWNLOAD=32
+readonly SCAN_WORKERS=1500
 readonly SCHEME='http'
 readonly PORT=80
-readonly SCAN_WORKERS=1500
 readonly UA='Mozilla/5.0'
 
 export SCHEME PORT EXTENSIONS UA
 
 random_sites() {
-    nmap -T5 --min-parallelism $SCAN_WORKERS --host-timeout 1s --max-retries 1 \
+    nmap -T5 --min-parallelism $SCAN_WORKERS --host-timeout 1s \
         -n -Pn -iR 0 -p $PORT --open -oG - 2>/dev/null \
         | awk '/open/{print $2}'
 }
 
 has_index() {
-    curl -sm 5 -A "$UA" "$1" | fgrep 'Index of' >/dev/null
+    curl -sm 7 -A "$UA" "$1" | fgrep 'Index of' >/dev/null
 }
 
 download() {
@@ -38,4 +39,4 @@ process() {
 
 export -f process has_index download
 
-random_sites | xargs -P 16 -I {} bash -c 'process {}'
+random_sites | xargs -P $PARALLEL_SITES_DOWNLOAD -I {} bash -c 'process {}'
